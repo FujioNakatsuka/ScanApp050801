64667ffc7cbedd2242ce635dc2a83487b2f50f07
Fujio Nakatsuka <com.nifty.cbm01799>
2020-05-08 00:21:55 +0900

    Initial Commit

---
+++ ScanApp050801/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanApp050801
+//
+//  Created by 中塚富士雄 on 2020/05/08.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
